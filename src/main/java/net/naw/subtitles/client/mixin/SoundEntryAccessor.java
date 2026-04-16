a30cef6ebd89f4f6a21fd6b8382b7a9849e007e2
Naw7k <n_m_k0@hotmail.com>
2026-04-16 12:28:00 +0300

    Update SoundEntryAccessor.java

--- src/main/java/net/naw/subtitles/client/mixin/SoundEntryAccessor.java
+++ src/main/java/net/naw/subtitles/client/mixin/SoundEntryAccessor.java
@@ -1,26 +1,26 @@
 package net.naw.subtitles.client.mixin;
 
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 /**
- * SoundEntryAccessor: The "Secret Key" for individual sounds.
- * Minecraft's SoundEntry class is 'private' (locked). This interface
+ * SoundEntryAccessor: The "Secret Key" for individual sound locations.
+ * Minecraft's SoundPlayedAt class is private (locked). This interface
  * lets us reach inside and grab the position and timing of a sound.
  */
-@Mixin(targets = "net.minecraft.client.gui.hud.SubtitlesHud$SoundEntry")
+@Mixin(targets = "net.minecraft.client.gui.components.SubtitleOverlay$SoundPlayedAt")
 public interface SoundEntryAccessor {
 
     /**
-     * This lets us grab the 3D position (X, Y, Z) of where a sound happened.
+     * Grabs the 3D position (X, Y, Z) of where a sound happened.
      * We need this to draw the little '<' or '>' arrows pointing to the sound!
      */
     @Accessor("location")
-    Vec3d getLocation();
+    Vec3 getLocation();
 
     /**
-     * This lets us grab the exact time the sound started.
+     * Grabs the exact time the sound started.
      * We use this to figure out when the subtitle should start fading away.
      */
     @Accessor("time")
